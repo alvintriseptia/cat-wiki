d200c157598faf3656a9c4574a52a39e2ead9dbf
alvintriseptia <alvintryseptia@gmail.com>
2021-12-29 14:01:35 +0700

    updated responsive breed description site

--- components/Score/Score.js
+++ components/Score/Score.js
@@ -5,43 +5,43 @@ const Score = ({ count }) => {
 		<div className="flex space-x-2">
 			{count === 1 ? (
 				<>
-					<div className="w-14 h-3 bg-[#544439] rounded-lg"></div>
-					<div className="w-14 h-3 bg-gray-200 rounded-lg"></div>
-					<div className="w-14 h-3 bg-gray-200 rounded-lg"></div>
-					<div className="w-14 h-3 bg-gray-200 rounded-lg"></div>
-					<div className="w-14 h-3 bg-gray-200 rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-[#544439] rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-gray-200 rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-gray-200 rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-gray-200 rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-gray-200 rounded-lg"></div>
 				</>
 			) : count === 2 ? (
 				<>
-					<div className="w-14 h-3 bg-[#544439] rounded-lg"></div>
-					<div className="w-14 h-3 bg-[#544439] rounded-lg"></div>
-					<div className="w-14 h-3 bg-gray-200 rounded-lg"></div>
-					<div className="w-14 h-3 bg-gray-200 rounded-lg"></div>
-					<div className="w-14 h-3 bg-gray-200 rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-[#544439] rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-[#544439] rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-gray-200 rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-gray-200 rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-gray-200 rounded-lg"></div>
 				</>
 			) : count === 3 ? (
 				<>
-					<div className="w-14 h-3 bg-[#544439] rounded-lg"></div>
-					<div className="w-14 h-3 bg-[#544439] rounded-lg"></div>
-					<div className="w-14 h-3 bg-[#544439] rounded-lg"></div>
-					<div className="w-14 h-3 bg-gray-200 rounded-lg"></div>
-					<div className="w-14 h-3 bg-gray-200 rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-[#544439] rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-[#544439] rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-[#544439] rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-gray-200 rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-gray-200 rounded-lg"></div>
 				</>
 			) : count === 4 ? (
 				<>
-					<div className="w-14 h-3 bg-[#544439] rounded-lg"></div>
-					<div className="w-14 h-3 bg-[#544439] rounded-lg"></div>
-					<div className="w-14 h-3 bg-[#544439] rounded-lg"></div>
-					<div className="w-14 h-3 bg-[#544439] rounded-lg"></div>
-					<div className="w-14 h-3 bg-gray-200 rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-[#544439] rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-[#544439] rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-[#544439] rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-[#544439] rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-gray-200 rounded-lg"></div>
 				</>
 			) : (
 				<>
-					<div className="w-14 h-3 bg-[#544439] rounded-lg"></div>
-					<div className="w-14 h-3 bg-[#544439] rounded-lg"></div>
-					<div className="w-14 h-3 bg-[#544439] rounded-lg"></div>
-					<div className="w-14 h-3 bg-[#544439] rounded-lg"></div>
-					<div className="w-14 h-3 bg-[#544439] rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-[#544439] rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-[#544439] rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-[#544439] rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-[#544439] rounded-lg"></div>
+					<div className="w-8 lg:w-14 h-3 bg-[#544439] rounded-lg"></div>
 				</>
 			)}
 		</div>
